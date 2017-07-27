60468d5143c5dca6d3fa5a32ff256d5d3a1b5279
zhangchx <zhangchx@sh-stt.com>
2017-07-27 14:28:58 +0800

    test
    Change-Id: I9101ba5723ef086909e5cdf937a59970d3a1d85e

---
+++ aaaa/src/main/java/as/aaaa/App.java
@@ -0,0 +1,13 @@
+package as.aaaa;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
